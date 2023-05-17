7b2222b978155e366ef4d5a17b062b6caaecffd3
apolyeti <azhandarveen@gmail.com>
2023-05-16 22:02:01 -0700

    Add stopwatch component, soon to integrate with counter

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,14 @@
 import { Center, Button, Text, Box } from "@chakra-ui/react";
 import { useState } from "react";
 import Counter from "./Counter";
+import Stopwatch from "./Stopwatch";
 
 export default function Hero() {
     const [count, setCount] = useState(0);
     return (
         <Center>
             <Counter />
+            <Stopwatch />
         </Center>
     );
 }
---
+++ src/components/Stopwatch.tsx
@@ -0,0 +1,45 @@
+import { use, useEffect, useState } from 'react';
+import { Button } from '@chakra-ui/react';
+
+export default function Stopwatch() {
+    const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+    useEffect(() => {
+        let interval : any;
+        if (running) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        } else if (!running) {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval);
+    }, [running]);
+    return (
+        <div>
+            <div>
+                <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+                <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+            </div>
+            <div>
+                <Button 
+                    onClick={() => setRunning(!running)}
+                    bgColor={'#7d476e'}
+                    _hover={{
+                        bgColor: '#9c608b',
+                    }}>
+                        {running ? "Stop" : "Start"}   
+                </Button>
+                <Button 
+                    onClick={() => setTime(0)}
+                    bgColor={'#7d476e'}
+                    _hover={{
+                        bgColor: '#9c608b',
+                    }}>
+                        Reset
+                </Button>
+            </div>
+        </div>
+    );
+}
